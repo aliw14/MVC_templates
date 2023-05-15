82f536be1aaf5d585a84e651e7092d23b69e5d84
ali <ali@MacBook-Pro-Ali.local>
2023-05-15 19:15:10 +0400

    message

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MVC_TEMPLATESs;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddMvc();
+
+        var app = builder.Build();
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+
+        app.UseEndpoints(endpoint =>
+        {
+            endpoint.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}");
+        });
+
+        app.Run();
+    }
+}
+
